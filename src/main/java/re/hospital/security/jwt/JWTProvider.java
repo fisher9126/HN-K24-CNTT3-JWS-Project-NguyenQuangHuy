c013aa04d62b23b0854adf402469c3e581eec3fc
cccc9126 <huyhuy9126@gmail.com>
2026-06-13 14:28:56 +0700

    done

--- src/main/java/re/hospital/security/jwt/JWTProvider.java
+++ src/main/java/re/hospital/security/jwt/JWTProvider.java
@@ -35,6 +35,7 @@ public class JWTProvider {
 
     public boolean validateToken(String token) {
         try {
+
             Jwts.parser().verifyWith(getSigningKey()).build().parseSignedClaims(token);
             return true;
         } catch (ExpiredJwtException e) {
